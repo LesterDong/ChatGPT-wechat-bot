298a188f9a028daf52f6336d2895f70935eecc42
donggang <donggang@chinagpay.com>
2023-02-07 01:20:02 +0800

    fix: api key

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export default {
   // 填入你的session token
-  OPENAI_API_KEY: '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',
+  OPENAI_API_KEY: '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',
   // 设置获取消息的重试次数
   retryTimes: 3,
   // 在群组中设置唤醒微信机器人的关键词
