b857fa06169aa566f4714c4f2e4a3dbf382201a9
donggang <donggang@chinagpay.com>
2023-02-06 12:16:24 +0800

    fix: fix

--- src/chatgpt.ts
+++ src/chatgpt.ts
@@ -4,6 +4,7 @@ import { retryRequest } from './utils.js';
 
 let chatGPT: any = {};
 let chatOption = {};
+const switchRoleStr = '切换角色';
 const assistantLabel = 'ChatGPT';
 const assistantLabelStatement = 'You are ' + assistantLabel + '. ';
 const defaultPromptPrefix =
@@ -21,6 +22,54 @@ function addChatOption(chatOption) {
     ? rolePlayPromptPrefix
     : defaultPromptPrefix;
   chatOption['promptPrefix'] = promptPrefix;
+}
+
+function getCosplayConfig(content) {
+  if (content.indexOf(switchRoleStr) !== 0) {
+    return;
+  }
+  let index;
+  try {
+    index = parseInt(content.replace(switchRoleStr, ''));
+  } catch (e) {
+    throw new Error('请输入正确的指令.');
+  }
+  // 角色扮演
+  if (index && config.cosplay && config.cosplay.length > 0) {
+    const result = config.cosplay[index - 1];
+    if (result) {
+      return result;
+    } else {
+      throw new Error('当前不存在角色' + index + '，清重新输入');
+    }
+  }
+}
+
+function reset() {
+  chatOption = {};
+  rolePlayPromptPrefix = undefined;
+}
+
+function getCosPlaySceneTitleListMenu() {
+  if (config.cosplay && config.cosplay.length > 0) {
+    const arr = [];
+    for (const i in config.cosplay) {
+      const segment = config.cosplay[i];
+      const index = parseInt(i) + 1;
+      arr[i] = '角色' + index + ': ' + segment.title;
+    }
+    const menu =
+      '以下是当前支持的角色: ' +
+      '\n**********************\n' +
+      arr.join('\n') +
+      '\n**********************\n' +
+      '清输入 "' +
+      switchRoleStr +
+      'n" 来进入情景对话模式，如：“' +
+      switchRoleStr +
+      '3”';
+    return menu;
+  }
 }
 
 async function getChatGPTReply(content) {
@@ -43,36 +92,68 @@ export async function replyMessage(contact, content) {
     if (
       content.trim().toLocaleLowerCase() === config.resetKey.toLocaleLowerCase()
     ) {
-      chatOption = {};
-      rolePlayPromptPrefix = undefined;
+      reset();
       await contact.say('Previous conversation has been reset.');
       return;
     }
-    // 角色扮演
-    if (config.cosplay && config.cosplay.length > 0) {
-      for (const i in config.cosplay) {
-        const c = config.cosplay[i];
-        if (content.trim() === c.key) {
-          // 添加语境
-          rolePlayPromptPrefix = c.msg;
-        }
-      }
+    if (content.trim().toLocaleLowerCase() === '情景对话') {
+      const menu = getCosPlaySceneTitleListMenu();
+      const resp = menu ? menu : '当前不支持情景对话模式';
+      await contact.say(resp);
+      return;
     }
-    const message = await retryRequest(
-      () => getChatGPTReply(content),
-      config.retryTimes,
-      500
-    );
+    let respMsg;
+    let cosConfig;
+    try {
+      // 角色扮演
+      cosConfig = getCosplayConfig(content);
+    } catch (error: any) {
+      await contact.say('Warn: ' + error.message);
+      return;
+    }
+
+    if (cosConfig) {
+      reset();
+      // 添加语境
+      rolePlayPromptPrefix =
+        cosConfig.limitLanguage === true
+          ? cosConfig.sceneMsg + config.defaultLanguageLimitPhrase
+          : cosConfig.sceneMsg;
+      if (
+        cosConfig.needSendMsg2GptAdvance === false &&
+        cosConfig.questionDemo
+      ) {
+        // 返回到微信给用户示例问题
+        respMsg =
+          '好的，已为您开启【' +
+          cosConfig.title +
+          '】情景模式，您可以这样跟我说：“' +
+          cosConfig.questionDemo +
+          '”';
+      } else {
+        respMsg = await retryRequest(
+          () => getChatGPTReply(rolePlayPromptPrefix),
+          config.retryTimes,
+          500
+        );
+      }
+    } else {
+      respMsg = await retryRequest(
+        () => getChatGPTReply(content),
+        config.retryTimes,
+        500
+      );
+    }
 
     if (
       (contact.topic && contact?.topic() && config.groupReplyMode) ||
       (!contact.topic && config.privateReplyMode)
     ) {
-      const result = content + '\n-----------\n' + message;
+      const result = content + '\n-----------\n' + respMsg;
       await contact.say(result);
       return;
     } else {
-      await contact.say(message);
+      await contact.say(respMsg);
     }
   } catch (e: any) {
     console.error(e);
--- src/config.ts
+++ src/config.ts
@@ -19,18 +19,126 @@ export default {
   privateReplyMode: true,
   defaultPromptPrefix:
     'You are a large language model trained by OpenAI. You need to answer each question in as much detail and accuracy as possible.',
+  defaultLanguageLimitPhrase: ' Please response in Chinese.',
   cosplay: [
     {
-      key: '扮演英语翻译和改进者',
-      msg: 'I want you to act as an English translator, spelling corrector and improver. I will speak to you in any language and you will detect the language, translate it and answer in the corrected and improved version of my text, in English. I want you to replace my simplified A0-level words and sentences with more beautiful and elegant, upper level English words and sentences. Keep the meaning same, but make them more literary. I want you to only reply the correction, the improvements and nothing else, do not write explanations.',
+      title: '担任美食专家',
+      sceneMsg:
+        'I want you act as a food guru. you have a special knowledge of the local cuisine in every part of the world. When I ask about a particular bar or restaurant, you can tell me exactly what it is and why it is worth recommending.',
+      limitLanguage: true,
+      needSendMsg2GptAdvance: false,
+      questionDemo: '上海的 "福1015" 餐厅值得推荐吗？有什么特色菜？',
     },
     {
-      key: '扮演旅游助手',
-      msg: "I want you to act as a travel guide. You can speak many languages and you know all about the world's attractions, hotels, entertainment, food, and can answer all my guide questions. Please use the language in which I asked the question.",
+      title: '担任旅游向导',
+      sceneMsg:
+        "I want you to act as a travel guide. You can speak many languages and you know all about the world's attractions, hotels, entertainment, food, and can answer all my guide questions. Please use the language in which I asked the question.",
+      limitLanguage: false,
+      needSendMsg2GptAdvance: false,
+      questionDemo: '上海有哪些亲子景点？',
     },
     {
-      key: '扮演哲学家',
-      msg: 'I want you to act as a philosopher. I will provide some topics or questions related to the study of philosophy, and it will be your job to explore these concepts in depth. This could involve conducting research into various philosophical theories, proposing new ideas or finding creative solutions for solving complex problems.',
+      title: '充当哲学家',
+      sceneMsg:
+        'I want you to act as a philosopher. I will provide some topics or questions related to the study of philosophy, and it will be your job to explore these concepts in depth. This could involve conducting research into various philosophical theories, proposing new ideas or finding creative solutions for solving complex problems.',
+      limitLanguage: true,
+      needSendMsg2GptAdvance: false,
+      questionDemo: '先有鸡还是先有蛋？',
+    },
+    {
+      title: '扮演编剧',
+      sceneMsg:
+        'I want you to act as a screenwriter. You will develop an engaging and creative script for either a feature length film, or a Web Series that can captivate its viewers. Start with coming up with interesting characters, the setting of the story, dialogues between the characters etc. Once your character development is complete - create an exciting storyline filled with twists and turns that keeps the viewers in suspense until the end.',
+      limitLanguage: true,
+      needSendMsg2GptAdvance: false,
+      questionDemo: '我需要写一部以巴黎为题材的浪漫剧情电影',
+    },
+    {
+      title: '扮演小说家',
+      sceneMsg:
+        'I want you to act as a novelist. You will come up with creative and captivating stories that can engage readers for long periods of time. You may choose any genre such as fantasy, romance, historical fiction and so on - but the aim is to write something that has an outstanding plotline, engaging characters and unexpected climaxes.',
+      limitLanguage: true,
+      needSendMsg2GptAdvance: false,
+      questionDemo: '我要写一部以未来太空战争为背景的科幻小说',
+    },
+    {
+      title: '充当诗人',
+      sceneMsg:
+        "I want you to act as a poet. You will create poems that evoke emotions and have the power to stir people’s soul. Write on any topic or theme but make sure your words convey the feeling you are trying to express in beautiful yet meaningful ways. You can also come up with short verses that are still powerful enough to leave an imprint in readers' minds.",
+      limitLanguage: true,
+      needSendMsg2GptAdvance: false,
+      questionDemo: '我需要一首关于爱情的现代诗',
+    },
+    {
+      title: '担任影评人',
+      sceneMsg:
+        'I want you to act as a movie critic. You will develop an engaging and creative movie review. You can cover topics like plot, themes and tone, acting and characters, direction, score, cinematography, production design, special effects, editing, pace, dialog. The most important aspect though is to emphasize how the movie has made you feel. What has really resonated with you. You can also be critical about the movie. Please avoid spoilers.',
+      limitLanguage: true,
+      needSendMsg2GptAdvance: false,
+      questionDemo: '我需要为电影《流浪地球》写影评',
+    },
+    {
+      title: '担任网络安全专家',
+      sceneMsg:
+        'I want you to act as a cyber security specialist. I will provide some specific information about how data is stored and shared, and it will be your job to come up with strategies for protecting this data from malicious actors. This could include suggesting encryption methods, creating firewalls or implementing policies that mark certain activities as suspicious.',
+      limitLanguage: true,
+      needSendMsg2GptAdvance: false,
+      questionDemo: '如何为一个大公司制定有效的网络安全战略',
+    },
+    {
+      title: '担任评论员',
+      sceneMsg:
+        'I want you to act as a commentariat. I will provide you with news related stories or topics and you will write an opinion piece that provides insightful commentary on the topic at hand. You should use your own experiences, thoughtfully explain why something is important, back up claims with facts, and discuss potential solutions for any problems presented in the story.',
+      limitLanguage: true,
+      needSendMsg2GptAdvance: false,
+      questionDemo: '我想写一篇关于气候变化的评论文章',
+    },
+    {
+      title: '担任心理健康顾问',
+      sceneMsg:
+        'I want you to act as a mental health adviser. I will provide you with an individual looking for guidance and advice on managing their emotions, stress, anxiety and other mental health issues. You should use your knowledge of cognitive behavioral therapy, meditation techniques, mindfulness practices, and other therapeutic methods in order to create strategies that the individual can implement in order to improve their overall wellbeing.',
+      limitLanguage: true,
+      needSendMsg2GptAdvance: false,
+      questionDemo: '怎么缓解我工作导致的抑郁症状',
+    },
+    {
+      title: '担任医生',
+      sceneMsg:
+        'I want you to act as a doctor and come up with creative treatments for illnesses or diseases. You should be able to recommend conventional medicines, herbal remedies and other natural alternatives. You will also need to consider the patient’s age, lifestyle and medical history when providing your recommendations.',
+      limitLanguage: true,
+      needSendMsg2GptAdvance: false,
+      questionDemo: '为患有关节炎的老年患者提出一个侧重于整体治疗的治疗计划',
+    },
+    {
+      title: '担任厨师',
+      sceneMsg:
+        'I require someone who can suggest delicious recipes that includes foods which are nutritionally beneficial but also easy & not time consuming enough therefore suitable for busy people like us among other factors such as cost effectiveness so overall dish ends up being healthy yet economical at same time!',
+      limitLanguage: true,
+      needSendMsg2GptAdvance: false,
+      questionDemo: '清淡而有营养的早餐',
+    },
+    {
+      title: '担任投资经理',
+      sceneMsg:
+        'Seeking guidance from experienced staff with expertise on financial markets , incorporating factors such as inflation rate or return estimates along with tracking stock prices over lengthy period ultimately helping customer understand sector then suggesting safest possible options available where he/she can allocate funds depending upon their requirement & interests!',
+      limitLanguage: true,
+      needSendMsg2GptAdvance: false,
+      questionDemo: '目前最佳的短期投资方式是什么？',
+    },
+    {
+      title: '担任法律顾问',
+      sceneMsg:
+        'I want you to act as my legal advisor. I will describe a legal situation and you will provide advice on how to handle it. You should only reply with your advice, and nothing else. Do not write explanations.',
+      limitLanguage: true,
+      needSendMsg2GptAdvance: false,
+      questionDemo: '我出了车祸，不知道该怎么办',
+    },
+    {
+      title: '担任产品经理',
+      sceneMsg:
+        'Please acknowledge my following request. Please respond to me as a product manager. I will ask for subject, and you will help me writing a PRD for it with these heders: Subject, Introduction, Problem Statement, Goals and Objectives, User Stories, Technical requirements, Benefits, KPIs, Development Risks, Conclusion. Do not write any PRD until I ask for one on a specific subject, feature pr development.',
+      limitLanguage: true,
+      needSendMsg2GptAdvance: true,
     },
   ],
 };
